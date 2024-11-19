795a678202e9929d020d9824a9661797c037bca7
CMirianG <mc2021071083@virtual.upt.pe>
2024-11-19 12:27:11 -0500

    terraform

---
+++ infra/main.tf
@@ -0,0 +1,26 @@
+provider "aws" {
+  region         = "us-east-1"
+  access_key     = var.aws_access_key
+  secret_key     = var.aws_secret_key
+  token          = var.aws_session_token
+}
+
+# Recurso: Base de datos MySQL (RDS)
+resource "aws_db_instance" "rds_mysql" {
+  allocated_storage    = 20
+  engine               = "mysql"
+  engine_version       = "8.0.32"         # Versión compatible más reciente
+  instance_class       = "db.t3.micro"   # Clase de instancia compatible
+  db_name              = "powerby"
+  username             = "admin"
+  password             = "password123"
+  publicly_accessible  = true
+  skip_final_snapshot  = true
+  tags = {
+    Name = "PowerBy Database"
+  }
+}
+
+output "db_endpoint" {
+  value = aws_db_instance.rds_mysql.endpoint
+}
---
+++ infra/variables.tf
@@ -0,0 +1,11 @@
+variable "aws_access_key" {
+  description = "Access key for AWS"
+}
+
+variable "aws_secret_key" {
+  description = "Secret key for AWS"
+}
+
+variable "aws_session_token" {
+  description = "Session token for AWS"
+}
