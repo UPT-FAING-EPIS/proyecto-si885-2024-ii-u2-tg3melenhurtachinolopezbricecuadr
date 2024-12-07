5745edc4f5df9cbaa9c2d57cf26541e6fa255cb1
GabrielFMH <84587604+GabrielFMH@users.noreply.github.com>
2024-12-06 22:05:13 -0500

    Update main.tf

--- athena/main.tf
+++ athena/main.tf
@@ -1,54 +1,65 @@
-# Recurso para el bucket de S3 ya existente
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  cloud {
+    organization = "UPT_InteligenciaNegocios"
+    workspaces {
+      name = "learn-terraform-migrate"
+    }
+  }
+}
+
+# Variable para la longitud del nombre
+variable "name_length" {
+  description = "Longitud del nombre"
+  type        = number
+  default     = 5
+}
+
+
+
+# Recurso para el bucket de S3 existente
 resource "aws_s3_bucket" "bucket" {
   bucket = var.bucket_name
 }
 
-# Recurso para actualizar el archivo en el bucket S3
-resource "aws_s3_object" "dataset" {
+# Recurso para cargar el archivo dataset.csv en el bucket S3
+resource "aws_s3_object" "upload_dataset" {
   bucket       = aws_s3_bucket.bucket.bucket
   key          = "data/input/dataset.csv"
-  source       = "dataset.csv"  # Asegúrate de que esta ruta sea correcta
+  source       = "dataset.csv"  # Ruta al archivo local
   acl          = "private"
   content_type = "text/csv"
-  depends_on   = [aws_s3_bucket.bucket]
-  etag = filemd5("dataset.csv")  # Asegúrate de que se calcule el hash
 
-
-  # Se asegura de que se actualice el archivo incluso si no cambia su ETag
   lifecycle {
-    ignore_changes = [etag]  # Ignora el ETag para forzar la actualización
+    ignore_changes = [etag]  # Ignorar cambios en el ETag para evitar recreación innecesaria
   }
 }
 
-# Recurso para el Glue Crawler que usa una base de datos ya existente
+# Recurso para el Glue Crawler
 resource "aws_glue_crawler" "crawler" {
   name          = var.crawler_name
   role          = "arn:aws:iam::905418255636:role/LabRole"
-  database_name = var.database_name  # Referencia la base de datos existente
+  database_name = var.database_name
 
   s3_target {
     path = "s3://${aws_s3_bucket.bucket.bucket}/data/input/"
   }
 
   lifecycle {
-    prevent_destroy = true  # Evita la destrucción del crawler
+    prevent_destroy = true  # Evitar la destrucción accidental del crawler
   }
 }
 
-# Recurso para la consulta de Athena (ya existente)
+# Recurso para la consulta de Athena
 resource "aws_athena_named_query" "athena_query" {
   name      = "example_query"
   database  = var.database_name
-  query     = "SELECT * FROM ${var.database_name}.input LIMIT 200;"  # Ajusta la consulta si es necesario
+  query     = "SELECT * FROM ${var.database_name}.input LIMIT 200;"
   workgroup = "primary"
-}
-
-resource "null_resource" "upload_csv" {
-  triggers = {
-    file_hash = filemd5("dataset.csv")  # Detecta cambios en el archivo local
-  }
-
-  provisioner "local-exec" {
-    command = "aws s3 cp dataset.csv s3://${aws_s3_bucket.bucket.bucket}/data/input/dataset.csv"
-  }
 }
